41c964dbdb1b4ebf8215631e57feb6312fb14773
HugoLaurent <lauhugopro@gmail.com>
2024-08-16 16:36:43 +0200

    chore: Ajouter les composants InputTime, InputDate, InputTextNumber et InputTextArea pour les champs de saisie

---
+++ src/Components/Inputs/index.js
@@ -0,0 +1,17 @@
+import InputCheckbox from "./InputCheckbox";
+import InputDate from "./InputDate";
+import InputTextNumber from "./InputTextNumber";
+import InputFile from "./InputFile";
+import InputSelect from "./InputSelect";
+import InputTextArea from "./InputTextArea";
+import InputTime from "./InputTime";
+
+export {
+  InputCheckbox,
+  InputDate,
+  InputTextNumber,
+  InputFile,
+  InputSelect,
+  InputTextArea,
+  InputTime,
+};
