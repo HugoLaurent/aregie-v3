0b6b1e732aae1c9cddc639437d307ee6cd69caf9
HugoLaurent <lauhugopro@gmail.com>
2024-08-16 15:50:25 +0200

    chore: Update Recette page with button components and styling

--- src/Pages/Recette/Recette.jsx
+++ src/Pages/Recette/Recette.jsx
@@ -1,10 +1,40 @@
 import "./recette-style.css";
-import { Link } from "react-router-dom";
+
+import { ButtonIconText } from "./../../Components/Buttons";
+import { add, search, print } from "./../../assets/images";
+import { ListRecette } from "../../Components";
 
 export default function Recette() {
   return (
-    <section className="Recette">
-      <Link to="/recettes/ajouter-une-recette">Liste des recettes button</Link>
+    <section className="recette-container">
+      <h2 className="recette-title">Liste des recettes</h2>
+      <article className="recette__button-container">
+        <ButtonIconText
+          link="/recettes/ajouter-une-recette"
+          icon={add}
+          text="Ajouter une recette"
+          color="#0081E3"
+          textColor="#fff"
+        />
+        <div className="recette__button-container-right">
+          <ButtonIconText
+            icon={search}
+            text="Rechercher"
+            color="#fff"
+            textColor="#000"
+            hoverColor="rgba(128, 128, 128, 0.1)"
+          />
+          <ButtonIconText
+            icon={print}
+            text="Éditions"
+            color="#fff"
+            textColor="#000"
+            hoverColor="rgba(128, 128, 128, 0.1)
+"
+          />
+        </div>
+      </article>
+      <ListRecette />
     </section>
   );
 }
