71f7deae656bdd38d306a5932f515bca5e51d410
HugoLaurent <lauhugopro@gmail.com>
2024-08-16 15:53:20 +0200

    chore: Update ButtonIconText component to use react-router-dom and add hoverColor prop

--- src/Components/Buttons/ButtonIconText/ButtonIconText.jsx
+++ src/Components/Buttons/ButtonIconText/ButtonIconText.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import "./button-text-icon-style.css";
 
-import './button-text-icon-style.css';
+export default function ButtonIconText({
+  onClick,
+  icon,
+  alt,
+  text,
+  color,
+  link,
+  textColor,
+  hoverColor, // Ajout de la prop pour la couleur de survol
+}) {
+  // Crée un objet pour les styles en ligne
+  const buttonStyle = {
+    backgroundColor: color || "transparent",
+    color: textColor || "inherit",
+    "--hover-color": hoverColor || "rgba(255, 255, 255, 0.2)", // Utilise la prop hoverColor
+  };
 
-export default function ButtonIconText({ onClick, icon, alt, text }) {
+  // Détermine le composant à utiliser
+  const Component = link ? Link : "button";
+
   return (
-    <button onClick={onClick} className='button-icon-text-container'>
-      <img src={icon} alt={alt} className='icon' />
+    <Component
+      to={link}
+      onClick={onClick}
+      className="button-icon-text-container"
+      style={buttonStyle}
+    >
+      <img src={icon} alt={alt} className="icon" />
       <span>{text}</span>
-    </button>
+    </Component>
   );
 }
