d239528417b983ee034f50fad4a8b5b37ce159a0
HugoLaurent <lauhugopro@gmail.com>
2024-08-22 17:13:27 +0200

    chore: Update ReglementRecette component styles and total calculation

--- src/Pages/Recette/Components/ReglementRecette/ReglementRecette.jsx
+++ src/Pages/Recette/Components/ReglementRecette/ReglementRecette.jsx
@@ -1,44 +1,50 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from "react";
 import { penModif } from "../../../../assets/images";
 import "./reglement-recette-style.css";
 
 export default function ReglementRecette({
   formData,
   setShowModalReglement,
-  montantDepenseTotal,
-  setMontantDepenseTotal,
-  setFormData,
+  montantReglementTotal,
+  setMontantReglementTotal,
 }) {
+  // Calcul du montant total
+  const totalMontant = formData.depenseReglement.reduce((acc, depense) => {
+    return acc + Number(depense.montant);
+  }, 0);
+
+  // Mise à jour de l'état du montant total
+  useEffect(() => {
+    setMontantReglementTotal(totalMontant);
+  }, [formData.depenseReglement, setMontantReglementTotal, totalMontant]);
+
   return (
     <section className="reglement-recette__container">
-      <article className="budget-recette__header">
+      <article className="reglement-recette__header">
         <p className="first-column">Règlement</p>
         <p>Total</p>
       </article>
-      <article className="budget-recette__item">
-        <p className="first-column">007 - ANCV</p>
-        <p>20,00€</p>
-        <img src={penModif} alt="" />
-      </article>
-      <article className="budget-recette__item">
-        <p className="first-column">007 - ANCV</p>
-        <p>20,00€</p>
-        <img src={penModif} alt="" />
-      </article>
-      <article className="budget-recette__button-container">
+      {formData.depenseReglement.map((depense, index) => (
+        <article key={index} className="reglement-recette__item">
+          <p className="first-column">{depense.reglement}</p>
+          <p>{depense.montant}€</p>
+          <img src={penModif} alt="" />
+        </article>
+      ))}
+
+      <article className="reglement-recette__button-container">
         <button
-          onClick={() => {
-            setShowModalReglement(true);
-          }}
-          className="budget-recette__button"
+          onClick={() => setShowModalReglement(true)}
+          className="reglement-recette__button"
         >
           Ajouter un règlement
         </button>
       </article>
 
-      <article className="budget-recette__footer">
+      <article className="reglement-recette__footer">
         <p>Total des règlements</p>
-        <p>30,00€</p>
+        <p>{montantReglementTotal}€</p>
       </article>
     </section>
   );
