778a3a758d6a5be8bde0b590bb3cdb88a6373113
HugoLaurent <lauhugopro@gmail.com>
2024-08-23 10:32:12 +0200

    chore: Update MainModal component to include framer-motion animation

--- src/Components/Modals/MainModal.jsx
+++ src/Components/Modals/MainModal.jsx
@@ -1,10 +1,27 @@
+import { motion, AnimatePresence } from "framer-motion";
 import "./main-modal-style.css";
 
 export default function MainModal({ children, show }) {
-  if (!show) return null;
+  const modalVariants = {
+    hidden: { opacity: 0, y: "-50%", x: "-50%" },
+    visible: { opacity: 1, y: "-50%", x: "-50%" },
+    exit: { opacity: 0, y: "-50%", x: "-50%" },
+  };
+
   return (
-    <section className="modale-container">
-      <article className="modale-wrapper">{children}</article>
-    </section>
+    <AnimatePresence>
+      {show && (
+        <motion.section
+          className="modale-container"
+          variants={modalVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ duration: 0.3 }}
+        >
+          <motion.article className="modale-wrapper">{children}</motion.article>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 }
