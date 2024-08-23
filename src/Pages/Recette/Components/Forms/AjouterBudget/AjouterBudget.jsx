645b64fcec8556db2bd8e3471ca9eb3d9f50dc97
HugoLaurent <lauhugopro@gmail.com>
2024-08-23 11:57:55 +0200

    chore: Update form styles and validation in AjouterBudget and AjouterReglement components

--- src/Pages/Recette/Components/Forms/AjouterBudget/AjouterBudget.jsx
+++ src/Pages/Recette/Components/Forms/AjouterBudget/AjouterBudget.jsx
@@ -1,62 +1,125 @@
 /* eslint-disable react/prop-types */
 import "./ajouter-budget-style.css";
 import { useState } from "react";
-import { check, x } from "../../../../../assets/images";
+import { check, warningCircle, x } from "../../../../../assets/images";
 import { Switch } from "../../../../../Components";
 import { ButtonIconText } from "../../../../../Components/Buttons";
 
 export default function AjouterBudget({ setShowModalBudget, setFormData }) {
-  const [setShowNonVersable] = useState(false);
+  const [showNonVersable, setShowNonVersable] = useState(false);
   const [showDate, setShowDate] = useState(false);
   const [showCommentaire, setShowCommentaire] = useState(false);
+  const [prixTotal, setPrixTotal] = useState(0);
+  const [quantite, setQuantite] = useState("");
+  const [prixUnitaire, setPrixUnitaire] = useState("");
+  const [budget, setBudget] = useState("");
+  const [modele, setModele] = useState("");
+  const [date, setDate] = useState("");
+  const [commentaire, setCommentaire] = useState("");
+  const [invalidModele, setInvalidModele] = useState(false);
+  const [invalidBudget, setInvalidBudget] = useState(false);
+  const [invalidQuantite, setInvalidQuantite] = useState(false);
+  const [invalidPrixUnitaire, setInvalidPrixUnitaire] = useState(false);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "quantite") {
+      setQuantite(value);
+      setPrixTotal(value * prixUnitaire);
+    } else if (id === "prix unitaire") {
+      setPrixUnitaire(value);
+      setPrixTotal(quantite * value);
+    }
+  };
 
   const handleBudgetSubmit = () => {
-    const budget = document.getElementById("budget").value;
-    const quantite = document.getElementById("quantite").value;
-    const prixUnitaire = document.getElementById("prix unitaire").value;
+    if (modele === "") {
+      setInvalidModele(true);
+      setTimeout(() => setInvalidModele(false), 3000);
+      return;
+    }
+
+    if (budget === "") {
+      setInvalidBudget(true);
+      setTimeout(() => setInvalidBudget(false), 3000);
+      return;
+    }
+
+    if (quantite === "") {
+      setInvalidQuantite(true);
+      setTimeout(() => setInvalidQuantite(false), 3000);
+      return;
+    }
+
+    if (prixUnitaire === "") {
+      setInvalidPrixUnitaire(true);
+      setTimeout(() => setInvalidPrixUnitaire(false), 3000);
+      return;
+    }
 
     setFormData((prevFormData) => ({
       ...prevFormData,
       budget: [
         ...prevFormData.budget,
         {
+          id: prevFormData.budget.length + 1,
+          modele,
           budget,
           quantite,
           prixUnitaire,
+          date,
+          nonVersable: showNonVersable,
+          commentaire,
         },
       ],
     }));
+
     setShowModalBudget(false);
   };
+
+  const canValidate =
+    modele !== "" && budget !== "" && quantite !== "" && prixUnitaire !== "";
 
   return (
     <section className="ajouter-budget__container">
       <section className="ajouter-budget__form">
         <article className="ajouter-budget__header">
           <h3>Ajouter une ligne budgétaire</h3>
-          <button
-            onClick={() => {
-              setShowModalBudget(false);
-            }}
-          >
-            <img src={x} alt="" />
+          <button type="button" onClick={() => setShowModalBudget(false)}>
+            <img src={x} alt="Close" />
           </button>
         </article>
         <article className="ajouter-budget__text-info">
           <div>
             <label htmlFor="modele">Modèle</label>
-            <select type="text" id="modele">
+            <select
+              id="modele"
+              value={modele}
+              onChange={(e) => setModele(e.target.value)}
+              className={invalidModele ? "invalid" : ""}
+            >
+              <option value="" disabled>
+                Choisissez un modèle.
+              </option>
               <option value="Boutique">Boutique</option>
               <option value="Entrée">Entrée</option>
             </select>
           </div>
           <div>
             <label htmlFor="budget">Budget</label>
-            <select type="text" id="budget">
+            <select
+              id="budget"
+              value={budget}
+              onChange={(e) => setBudget(e.target.value)}
+              className={invalidBudget ? "invalid" : ""}
+            >
+              <option value="" disabled>
+                Choisissez un budget.
+              </option>
               <option value="ENFJ - 211 - 7067 - Participation des familles aux accueils">
                 ENFJ - 211 - 7067 - Participation des familles aux accueils
               </option>
-              <option value=" ENHG - 222 - 7654 - JE ne sais pas trop quoi mettre">
+              <option value="ENHG - 222 - 7654 - JE ne sais pas trop quoi mettre">
                 ENHG - 222 - 7654 - JE ne sais pas trop quoi mettre
               </option>
             </select>
@@ -65,21 +128,37 @@ export default function AjouterBudget({ setShowModalBudget, setFormData }) {
         <article className="ajouter-budget__number-info">
           <div>
             <label htmlFor="quantite">Quantité</label>
-            <input type="number" id="quantite" placeholder="0" />
+            <input
+              type="number"
+              id="quantite"
+              placeholder="0"
+              value={quantite}
+              onChange={handleChange}
+              className={invalidQuantite ? "invalid" : ""}
+            />
           </div>
           <div>
             <label htmlFor="prix unitaire">Prix unitaire</label>
-            <input type="number" id="prix unitaire" placeholder="0,00" />
+            <input
+              type="number"
+              id="prix unitaire"
+              placeholder="0,00"
+              value={prixUnitaire}
+              onChange={handleChange}
+              className={invalidPrixUnitaire ? "invalid" : ""}
+            />
           </div>
           <div>
             <label htmlFor="total">Prix Total</label>
             <div className="input-number__container">
               <p>€</p>
               <input
+                disabled
                 className="input-number"
                 type="number"
                 id="total"
-                placeholder="0,00"
+                value={prixTotal}
+                placeholder={prixTotal}
               />
             </div>
           </div>
@@ -91,12 +170,18 @@ export default function AjouterBudget({ setShowModalBudget, setFormData }) {
               <Switch setShow={setShowDate} show={showDate} />
               <p>Ajouter une date</p>
             </div>
-            {showDate && <input type="date" id="date-debut" />}
+            {showDate && (
+              <input
+                type="date"
+                id="date-debut"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            )}
           </article>
           <hr />
           <article className="ajouter-budget__non-versbale">
             <div className="ajouter-budget-label">
-              <Switch setShow={setShowNonVersable} show={setShowNonVersable} />
+              <Switch setShow={setShowNonVersable} show={showNonVersable} />
               <p>Ligne budgétaire non-versable</p>
             </div>
           </article>
@@ -106,21 +191,27 @@ export default function AjouterBudget({ setShowModalBudget, setFormData }) {
               <Switch setShow={setShowCommentaire} show={showCommentaire} />
               <p>Commentaires</p>
             </div>
-            {showCommentaire && <textarea id="commentaire" />}
+            {showCommentaire && (
+              <textarea
+                id="commentaire"
+                value={commentaire}
+                onChange={(e) => setCommentaire(e.target.value)}
+              />
+            )}
           </article>
         </section>
         <section className="ajouter-budget__footer">
           <ButtonIconText
-            type={"button"}
+            type="button"
             text="Annuler"
             onClick={() => setShowModalBudget(false)}
           />
           <ButtonIconText
-            type={"button"}
-            icon={check}
+            type="button"
+            icon={!canValidate ? warningCircle : check}
             text="Valider"
-            color={"rgba(107, 114, 128, 0.3)"}
-            textColor={"#fff"}
+            color={!canValidate ? "rgb(255, 165, 0)" : "#00A7DC"}
+            textColor="#fff"
             onClick={handleBudgetSubmit}
           />
         </section>
--- src/Pages/Recette/Components/Forms/AjouterReglement/AjouterReglement.jsx
+++ src/Pages/Recette/Components/Forms/AjouterReglement/AjouterReglement.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
-// Parent Component: AjouterRecette
 import "./ajouter-reglement-style.css";
 import { useState } from "react";
-import { check, x } from "../../../../../assets/images";
+import { check, warningCircle, x } from "../../../../../assets/images";
 import { Switch } from "../../../../../Components";
 import { ButtonIconText } from "../../../../../Components/Buttons";
 
@@ -10,19 +9,38 @@ export default function AjouterReglement({
   setShowModalReglement,
   setFormData,
 }) {
-  const [setShowNonVersable] = useState(false);
+  const [reglement, setReglement] = useState("");
+  const [montant, setMontant] = useState("");
+  const [invalidReglement, setInvalidReglement] = useState(false);
+  const [invalidMontant, setInvalidMontant] = useState(false);
+  const [showTiersPayeur, setShowTiersPayeur] = useState(false);
+  const [showNonVersable, setShowNonVersable] = useState(false);
+  const [showReferencesExternes, setShowReferencesExternes] = useState(false);
 
   const handleRecetteSubmit = () => {
-    const reglement = document.getElementById("reglement").value;
-    const montant = document.getElementById("montant").value;
+    if (reglement === "") {
+      setInvalidReglement(true);
+      setTimeout(() => setInvalidReglement(false), 3000);
+      return;
+    }
+
+    if (montant === "") {
+      setInvalidMontant(true);
+      setTimeout(() => setInvalidMontant(false), 3000);
+      return;
+    }
 
     setFormData((prevFormData) => ({
       ...prevFormData,
       reglement: [
         ...prevFormData.reglement,
         {
+          id: prevFormData.reglement.length + 1,
           reglement,
           montant,
+          tiersPayeur: showTiersPayeur,
+          nonVersable: showNonVersable,
+          referencesExternes: showReferencesExternes,
         },
       ],
     }));
@@ -34,18 +52,22 @@ export default function AjouterReglement({
       <section className="ajouter-reglement__form">
         <article className="ajouter-reglement__header">
           <h3>Ajouter un règlement</h3>
-          <button
-            onClick={() => {
-              setShowModalReglement(false);
-            }}
-          >
-            <img src={x} alt="" />
+          <button type="button" onClick={() => setShowModalReglement(false)}>
+            <img src={x} alt="Close" />
           </button>
         </article>
         <article className="ajouter-reglement__text-info">
           <div>
             <label htmlFor="reglement">Modèle</label>
-            <select type="text" id="reglement">
+            <select
+              id="reglement"
+              value={reglement}
+              onChange={(e) => setReglement(e.target.value)}
+              className={invalidReglement ? "invalid" : ""}
+            >
+              <option value="" disabled>
+                Choisissez un modèle.
+              </option>
               <option value="001-Carte Bancaire">001-Carte Bancaire</option>
               <option value="002-Espèce">002-Espèce</option>
             </select>
@@ -55,10 +77,12 @@ export default function AjouterReglement({
             <div className="input-number__container">
               <p>€</p>
               <input
-                className="input-number"
                 type="number"
                 id="montant"
                 placeholder="0,00"
+                value={montant}
+                onChange={(e) => setMontant(e.target.value)}
+                className={invalidMontant ? "invalid" : ""}
               />
             </div>
           </div>
@@ -67,37 +91,40 @@ export default function AjouterReglement({
           <hr />
           <article className="ajouter-reglement__add-date">
             <div className="ajouter-reglement-label">
-              <Switch />
+              <Switch setShow={setShowTiersPayeur} show={showTiersPayeur} />
               <p>Ajouter un tiers-payeur</p>
             </div>
           </article>
           <hr />
           <article className="ajouter-reglement__non-versbale">
             <div className="ajouter-reglement-label">
-              <Switch setShow={setShowNonVersable} show={setShowNonVersable} />
+              <Switch setShow={setShowNonVersable} show={showNonVersable} />
               <p>Ligne budgétaire non-versable</p>
             </div>
           </article>
           <hr />
           <article className="ajouter-reglement__commentaire">
             <div className="ajouter-reglement-label">
-              <Switch />
+              <Switch
+                setShow={setShowReferencesExternes}
+                show={showReferencesExternes}
+              />
               <p>Modifier les références externes</p>
             </div>
           </article>
         </section>
         <section className="ajouter-reglement__footer">
           <ButtonIconText
-            type={"button"}
+            type="button"
             text="Annuler"
             onClick={() => setShowModalReglement(false)}
           />
           <ButtonIconText
-            type={"button"}
-            icon={check}
+            type="button"
+            icon={reglement && montant ? check : warningCircle}
             text="Valider"
-            color={"rgba(107, 114, 128, 0.3)"}
-            textColor={"#fff"}
+            color={reglement && montant ? "#00A7DC" : "rgb(255, 165, 0)"}
+            textColor="#fff"
             onClick={handleRecetteSubmit}
           />
         </section>
